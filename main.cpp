b874b0a98fc0e374ec2db4d6706aa09e7b26b5da
alexbuzzbee <alexbuzzbee@gmail.com>
2014-08-25 15:18:52 -0400

    Added state_init() function.

--- main.cpp
+++ main.cpp
@@ -5,11 +5,24 @@
 #include "misc.hpp"
 //#include "main.hpp"
 
+void state_init(lua_State *L) {
+  luaL_openlibs(L);
+  l_misc_init(L);
+  // Sandboxing: remove loadfile(), print(), dofile(), and collectgarbage().
+  lua_pushnil(L);
+  lua_setglobal(L, "loadfile");
+  lua_pushnil(L);
+  lua_setglobal(L, "print");
+  lua_pushnil(L);
+  lua_setglobal(L, "dofile");
+  lua_pushnil(L);
+  lua_setglobal(L, "collectgarbage");
+}
+
 int main() {
   lua_State *L = luaL_newstate();
   printf("Lua OS; Lua version: %s R%s (modified).\n", LUA_VERSION, LUA_VERSION_RELEASE);
-  luaL_openlibs(L);
-  l_misc_init(L);
+  state_init(L);
   lua_close(L);
   return 0;
 }
